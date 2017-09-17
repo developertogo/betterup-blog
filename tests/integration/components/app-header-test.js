f2f499b5c76fba72bed7b2bf2565aed07d237eef
Tom Netzband <tomnez@gmail.com>
2017-09-17 15:24:24 -0400

    (tests) app-header integration test

--- tests/integration/components/app-header-test.js
+++ tests/integration/components/app-header-test.js
@@ -6,19 +6,7 @@ moduleForComponent('app-header', 'Integration | Component | app header', {
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{app-header}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#app-header}}
-      template block text
-    {{/app-header}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.ok(this.$('.logo').length, 'app-header component renders');
 });
