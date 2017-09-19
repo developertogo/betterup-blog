cf744b6ee5382b1e90e544c160048841b0131b64
Tom Netzband <tomnez@gmail.com>
2017-09-18 20:51:31 -0400

    (tests) betterup-modal/container integration tests

--- tests/integration/components/betterup-modal/container-test.js
+++ tests/integration/components/betterup-modal/container-test.js
@@ -1,24 +1,37 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import testSelector from 'ember-test-selectors';
 
 moduleForComponent('betterup-modal/container', 'Integration | Component | betterup modal/container', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('it renders block content', function(assert) {
+  assert.expect(1);
 
-  this.render(hbs`{{betterup-modal/container}}`);
+  this.set('closeAction', () => {});
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
   this.render(hbs`
-    {{#betterup-modal/container}}
-      template block text
+    {{#betterup-modal/container close=(action closeAction)}}
+      <h1 data-test-modal-content>Hi!</h1>
     {{/betterup-modal/container}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$(testSelector('modal-content')).length, 1, 'block content is rendered');
+});
+
+test('it calls a close action when the close icon is clicked', function(assert) {
+  assert.expect(1);
+
+  this.set('closeAction', () => {
+    assert.ok(true, 'the close action is called when the close icon is clicked on');
+  });
+
+  this.render(hbs`
+    {{#betterup-modal/container close=(action closeAction)}}
+      <h1>Hi!</h1>
+    {{/betterup-modal/container}}
+  `);
+
+  this.$(testSelector('modal-container-close')).click();
 });
