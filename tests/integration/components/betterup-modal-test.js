746297c828a47b2b81bfe98e8a53b409837cea8b
Tom Netzband <tomnez@gmail.com>
2017-09-18 20:19:39 -0400

    (tests) betterup-modal integration tests

--- tests/integration/components/betterup-modal-test.js
+++ tests/integration/components/betterup-modal-test.js
@@ -1,24 +1,57 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import testSelector from 'ember-test-selectors';
+import Ember from 'ember';
 
-moduleForComponent('betterup-modal', 'Integration | Component | betterup modal', {
+moduleForComponent('betterup-modal', 'Integration | Component | betterup modal farts', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+const { Component } = Ember;
 
-  this.render(hbs`{{betterup-modal}}`);
+test('it does not display a modal when isOpen is false', function(assert) {
+  this.register('component:integration-test-modal', Component.extend({
+    layout: hbs`<h1 data-test-modal>test-modal</h1>`
+  }));
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
   this.render(hbs`
-    {{#betterup-modal}}
-      template block text
-    {{/betterup-modal}}
+    {{betterup-modal
+      isOpen=false
+      modal=(hash component='integration-test-modal')
+    }}
+    {{betterup-modal/target}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$(testSelector('modal')).length, 0,  'modal is not rendered');
+});
+
+test('it does display a passed-in modal component when isOpen is set to true', function(assert) {
+  this.register('component:integration-test-modal', Component.extend({
+    layout: hbs`<h1 data-test-modal>test-modal</h1>`
+  }));
+
+  this.render(hbs`
+    {{betterup-modal
+      isOpen=true
+      modal=(hash component='integration-test-modal')
+    }}
+    {{betterup-modal/target}}
+  `);
+
+  assert.equal(this.$(testSelector('modal')).length, 1,  'modal is rendered');
+});
+
+test('arbitrarily named properties can be passed into the betterup-modal hash and be used in the rendered component', function(assert) {
+  this.register('component:integration-test-modal', Component.extend({
+    layout: hbs`<h1 data-test-modal>{{modal.name}}</h1>`
+  }));
+
+  this.render(hbs`
+    {{betterup-modal
+      isOpen=true
+      modal=(hash component='integration-test-modal' name="super neato name")}}
+    {{betterup-modal/target}}
+  `);
+
+  assert.equal(this.$(testSelector('modal')).text().trim(), 'super neato name', 'passing properties to the passed-in component works');
 });
