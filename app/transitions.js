09d3c87b26a191f859c9dd781201b4a1a1d51ac8
Tom Netzband <tomnez@gmail.com>
2017-09-20 17:17:33 -0400

    (feature) use ease out expo bezier for easing

--- app/transitions.js
+++ app/transitions.js
@@ -2,7 +2,7 @@ export default function(){
   this.transition(
     this.fromRoute('posts.index'),
     this.toRoute('posts.show'),
-    this.use('toLeft', { duration: 300 }),
-    this.reverse('toRight', { duration: 300 })
+    this.use('toLeft', { duration: 600, easing: [0.19, 1, 0.22, 1]}),
+    this.reverse('toRight', { duration: 600, easing: [0.19, 1, 0.22, 1]})
   );
 }
