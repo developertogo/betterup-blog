4521943760a6cafcd4c1ac15ef4d15836398cdaa
Tom Netzband <tomnez@gmail.com>
2017-09-20 16:54:33 -0400

    (tests) integration test for loading-indicator

--- tests/integration/components/loading-indicator-test.js
+++ tests/integration/components/loading-indicator-test.js
@@ -1,25 +1,20 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import testSelector from 'ember-test-selectors';
 
 moduleForComponent('loading-indicator', 'Integration | Component | loading indicator', {
   integration: true
 });
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
+test('it renders without any settings being passed in', function(assert) {
   this.render(hbs`{{loading-indicator}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$(testSelector('loader')).text().trim(), 'Loading...', 'loader renders');
+});
 
-  // Template block usage:
-  this.render(hbs`
-    {{#loading-indicator}}
-      template block text
-    {{/loading-indicator}}
-  `);
+test('it renders size and color when they are passed in', function(assert) {
+  this.render(hbs`{{loading-indicator size='50px' color='#000'}}`);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  let expectedStyle = 'font-size: 50px; color: #000';
+  assert.equal(this.$(testSelector('loader')).attr('style'), expectedStyle, 'it generates style based on passed-in attributes');
 });
